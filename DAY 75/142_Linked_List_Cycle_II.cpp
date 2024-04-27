b54704eb94ad5d9f4ebedb33498a255e6111c62b
Akash chaudhary <146649104+codewithAkash1326@users.noreply.github.com>
2024-04-27 19:43:53 +0530

    Add files via upload

---
+++ DAY 75/142_Linked_List_Cycle_II.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+  struct ListNode {
+      int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        bool ans = false;
+        ListNode* slow = head;
+         ListNode* fast = head;
+
+         while(fast!=NULL){
+            fast = fast->next;
+
+            if(fast!=NULL){
+                fast = fast->next;
+                slow = slow->next;
+
+                if(fast==slow){
+                    ans = true;
+                    break;
+                }
+            }
+
+        }
+
+        if(ans==true){
+            slow = head;
+
+            while(slow!=fast){
+                slow=slow->next;
+                fast=fast->next;
+            }
+
+            return slow;
+        }
+         else return NULL;
+
+    }
+};
+int main(){
+ 
+ 
+ 
+return 0;
+}
