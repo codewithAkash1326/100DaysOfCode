7c19de8febbd5c7a4d99325a6e8ade37b99c867c
Akash chaudhary <146649104+codewithAkash1326@users.noreply.github.com>
2024-04-27 19:44:41 +0530

    Add files via upload

---
+++ DAY 79/Next_Smaller_Element.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include<stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int>st;
+    st.push(-1);
+
+    vector<int>ans(n);
+
+    for(int i=arr.size()-1;i>=0;i--){
+ 
+    while(arr[i] <= st.top() && !st.empty() ){
+           st.pop();
+      }
+      
+      ans[i] = st.top();
+       st.push(arr[i]);
+ 
+ 
+    }
+
+
+
+    return ans;
+}
+int main(){
+ 
+ 
+ 
+return 0;
+}
