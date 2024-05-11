a20c833f245edbb82e7ff5a1fed0359a09c7b542
Akash chaudhary <146649104+codewithAkash1326@users.noreply.github.com>
2024-05-11 22:09:17 +0530

    Add files via upload

---
+++ DAY 86/Is_Height_Balanced_Binary_Tree.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+      
+
+    template <typename T>
+    class TreeNode
+    {
+    public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val)
+        {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+ 
+int height(TreeNode<int>* root){
+    if(root==NULL) return 0;
+
+    int lh = height(root->left);
+    int rh = height(root->right);
+
+    if(abs(lh-rh)>1) return -1;
+
+    if(lh==-1 || rh == -1) return -1;
+
+    return max(lh,rh)+1;
+}
+bool isBalancedBT(TreeNode<int>* root){
+    int ans = height(root);
+
+    if(ans!=-1) return true;
+    else return false;
+}
+int main(){
+ 
+ 
+ 
+return 0;
+}
